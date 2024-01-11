be03d64a580a4b2f851b2ad51b44562c035a79db
EC2 Default User <ec2-user@ip-172-31-20-190.ap-southeast-2.compute.internal>
2024-01-11 08:26:32 +0000

    テキストのLaravel 11 課題

--- routes/web.php
+++ routes/web.php
@@ -23,16 +23,22 @@ Route::controller(NewsController::class)
     Route::get('news/create','add');
 });
 
-
+use App\Http\Controllers\Admin\ProfileController;
 route::controller(ProfileController::class)
 ->prefix('admin')->group(function() {
     route::get('profile/create','add');
+    route::get('profile/edit','edit');
 });
 
+/*
 
 route::controller(ProfileController::class)
 ->prefix('admin')->group(function() {
     route::get('profile/edit','edit');
 });
 
+*/
 
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
