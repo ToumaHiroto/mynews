c9bd39b90794306a6be52f0e8398c4e68789d341
EC2 Default User <ec2-user@ip-172-31-20-190.ap-southeast-2.compute.internal>
2024-01-19 04:57:15 +0000

    テキストLaravel 19 課題

---
+++ resources/views/layouts/front.blade.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+
+<html lang="{{ app()->getLocale() }}">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-AU-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <!-- CSRF Token -->
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+        
+        <title>@yield('title')</title>
+        
+        <!-- Scripts -->
+        <script src="{{ secure_asset('js/app.js') }}"defer></script>
+        
+        <!-- Fonts -->
+        <link rel="dns-prefetch" href="https://fonts.gstatic.com">
+        <link href="https://fonts.googleapis.com/css? family=Raleway:300,400,600" rel="stylesheet" type="text/css">
+        
+        <!-- styles -->
+        <link href="{{ secure_asset('css/app.css') }}" rel="stylesheet">
+        <link href="{{ secure_asset('css/front.css') }}" rel="stylesheet">
+    </head>
+    <body>
+        <div id="app">
+            <nav class="navbar navbar-expand-md navbar-dark navbar-laravel">
+                <a class="navbar-brand" href="{{ url('/') }}">
+                    {{ config('app.name','Laravel') }}
+                </a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" 
+                        aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <!--Left Side of Navbar -->
+                    <ul class="navbar-nav ms-auto"></ul>
+                    
+                    <!-- Right Side of Navbar -->
+                    <ul class="navbar-nav">
+                        <!-- Authentication Links -->
+                        @guest
+                            <li><a class="mav-link" href="{{ route('login') }}">{{ __('messages.login') }}</a></li>
+                        @else
+                            <li class="nav-item dropdown">
+                                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                    
+                                    {{ Auth::user()->name }} <span class="caret"></span>
+                                    
+                                </a>
+                                
+                                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                                    <a class="dropdown-item" href="{{ route('logout') }}"
+                                       onclick="event.preventDefault();
+                                       document.getElementById('logout-form').submit();">
+                                        {{ __('messages.logout') }}
+                                    </a>
+                                    
+                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                        @csrf
+                                    </form>
+                                </div>
+                            </li>
+                        @endguest
+                    </ul>
+                </div>
+            </nav>
+            {{-- ここまでナビゲーションバー --}}
+            
+            <main class="py-4">
+                {{-- コンテンツをここに入れるため、@yieldで空けておく　--}}
+                @yield('content')
+            </main>
+        </div>
+    </body>
+</html>
